e479d4d8c18eec17f19dd48650bc45a69a7837ea
Herick Kumata <herickyk@gmail.com>
2024-04-10 19:56:43 -0300

    Alteração feira com a adição de pilhas, listas

--- SistemaDeRecomendacao.java
+++ SistemaDeRecomendacao.java
@@ -1,23 +1,28 @@
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
 
-
-//classe que representa o livro
+// Classe que representa o livro
 class Livro {
-    //node titulo
     private String titulo;
-    
-    //constructor
-    public Livro(String titulo) {
+    private int ano;
+    private String autor;
+
+    public Livro(String titulo, int ano, String autor) {
         this.titulo = titulo;
+        this.ano = ano;
+        this.autor = autor;
     }
 
     public String getTitulo() {
         return titulo;
     }
 
+    public int getAno() {
+        return ano;
+    }
+
+    public String getAutor() {
+        return autor;
+    }
 
     @Override
     public String toString() {
@@ -25,19 +30,14 @@ class Livro {
     }
 }
 
-//classe que representa um grafo de livros
+// Classe que representa um grafo de livros
 class GrafoDeLivros {
-
-    //grafo Map (Livro => Setado Livro)
     private Map<Livro, Set<Livro>> grafo;
 
-
-    //constructor do grafo de livros
     public GrafoDeLivros() {
         this.grafo = new HashMap<>();
     }
-    
-    //Método usado para adicionar uma relação entre um livro e o outro
+
     public void adicionarRelacao(Livro livro, Livro... recomendacoes) {
         Set<Livro> livrosRecomendados = new HashSet<>();
         for (Livro rec : recomendacoes) {
@@ -46,31 +46,28 @@ class GrafoDeLivros {
         grafo.put(livro, livrosRecomendados);
     }
 
-    //Método usado para usar as recomendações de um livro
     public Set<Livro> obterRecomendacoes(Livro livro) {
         return grafo.getOrDefault(livro, new HashSet<>());
     }
 
-    //Método para obter todos os livros no grafo
     public Set<Livro> obterTodosLivros() {
         return grafo.keySet();
     }
 }
 
 public class SistemaDeRecomendacao {
-
     public static void main(String[] args) {
-        // criando livros para os grafos
-        Livro livro1 = new Livro("Harry Potter: Ordem da Phoenix");
-        Livro livro2 = new Livro("World of WarCraft");
-        Livro livro3 = new Livro("IT: A coisa");
-        Livro livro4 = new Livro("1988: Steven King");
-        Livro livro5 = new Livro("Castelo de Cartas");
-        Livro livro6 = new Livro("Código da Vinte");
-        Livro livro7 = new Livro("Eu sou o número 4");
-        Livro livro8 = new Livro("Vikings");
-        Livro livro9 = new Livro("O Iluminado");
-        Livro livro10 = new Livro("Doutor Sono");
+        // Criando livros para os grafos
+        Livro livro1 = new Livro("Harry Potter: Ordem da Phoenix", 2003, "J.K. Rowling");
+        Livro livro2 = new Livro("World of WarCraft", 2004, "Chris Metzen");
+        Livro livro3 = new Livro("IT: A coisa", 1986, "Stephen King");
+        Livro livro4 = new Livro("1988: Steven King", 1988, "Stephen King");
+        Livro livro5 = new Livro("Castelo de Cartas", 1987, "Michael Dobbs");
+        Livro livro6 = new Livro("Código da Vinte", 2003, "Dan Brown");
+        Livro livro7 = new Livro("Eu sou o número 4", 2010, "Pittacus Lore");
+        Livro livro8 = new Livro("Vikings", 1951, "Johan Theorin");
+        Livro livro9 = new Livro("O Iluminado", 1977, "Stephen King");
+        Livro livro10 = new Livro("Doutor Sono", 2013, "Stephen King");
 
         // Criar o grafo de livros e adicionar relações
         GrafoDeLivros grafo = new GrafoDeLivros();
@@ -88,6 +85,28 @@ public class SistemaDeRecomendacao {
         for (Livro livro : grafo.obterTodosLivros()) {
             Set<Livro> recomendacoes = grafo.obterRecomendacoes(livro);
             System.out.println("Recomendações para " + livro.getTitulo() + ": " + recomendacoes);
+        }
+
+        // Adicionando uma pilha de livros
+        Stack<Livro> pilhaDeLivros = new Stack<>();
+        pilhaDeLivros.push(livro1);
+        pilhaDeLivros.push(livro2);
+        pilhaDeLivros.push(livro3);
+
+        System.out.println("Pilha de livros:");
+        while (!pilhaDeLivros.isEmpty()) {
+            System.out.println(pilhaDeLivros.pop());
+        }
+
+        // Adicionando uma lista de livros
+        List<Livro> listaDeLivros = new ArrayList<>();
+        listaDeLivros.add(livro4);
+        listaDeLivros.add(livro5);
+        listaDeLivros.add(livro6);
+
+        System.out.println("Lista de livros:");
+        for (Livro livro : listaDeLivros) {
+            System.out.println(livro);
         }
     }
 }
